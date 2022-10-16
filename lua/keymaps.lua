ca233e630fe72f2386f9bd775aea9e49e721d05e
Nahuel <nahuel.dev.23@gmail.com>
2022-10-16 14:01:57 -0300

    shortcut NERDTree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -21,5 +21,6 @@ map('n','<Leader>gb','<cmd>Fzf git_branches<cr>',{noremap=true}) -- fzf-lua
 
 --otros
 map('n','<Leader>b','<cmd>Fzf buffers<cr>',{noremap=true}) --ver buffers abiertos
+map('n','<Leader>n','cmd>NERDTree<cr>',{noremap=true}) --ver buffers abiertos
 
 map('n','<Leader>s',':call feedkeys("\\<Plug>(easymotion-s)")<CR>',{noremap=true})
